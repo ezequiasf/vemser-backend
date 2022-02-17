4368e6bbe293191f9dc10af0bb369c6f317a6465
ezequiasf <ezequiasbarros1997@gmail.com>
2022-02-16 21:58:07 -0300

    Exercícios da lista 1

---
+++ lista1/src/com/dbc/Exercicio6.java
@@ -0,0 +1,48 @@
+package com.dbc;
+
+import utils.Leitor;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Exercicio6 {
+
+    static List<String[]> palavraLista = Leitor.leitorArquivos("src/resources/palavras.txt");
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final String palavra;
+
+        System.out.println("Bem vindo ao tradutor");
+        System.out.println("Como você deseja traduzir? (1) por -> ing / (2) ing -> por");
+        int traducao = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Digite a palavra:");
+        palavra = sc.nextLine();
+
+        if (traducao == 1) {
+            tradutor(palavra,0);
+        }else if(traducao==2){
+            tradutor(palavra, 1);
+        }else{
+            System.out.println("Resposta inválida.");
+        }
+    }
+
+    private static void tradutor(String palavra, int index) {
+        int sentinela = (index ==0) ? 1:-1;
+        boolean existe = false;
+        for (String[] arr:palavraLista){
+            if(arr[index].equalsIgnoreCase(palavra)) {
+                System.out.println(arr[index + sentinela]);
+                existe = true;
+                break;
+            }
+        }
+        if (!existe){
+            System.out.println("Essa palavra não é válida.");
+        }
+    }
+
+}
