41fc6ba6a250871d6053601275235f5369ce8e57
Ezequias Ferreira <ezequiasbarros1997@gmail.com>
2022-03-29 06:47:44 -0300

    Organização da estrutura

---
+++ modulo3_homework3/vemser/src/main/java/com/dbccompany/vemser/exceptions/RegraDeNegocioException.java
@@ -0,0 +1,8 @@
+package com.dbccompany.vemser.exceptions;
+
+public class RegraDeNegocioException extends Exception{
+
+    public RegraDeNegocioException(String message) {
+        super(message);
+    }
+}
