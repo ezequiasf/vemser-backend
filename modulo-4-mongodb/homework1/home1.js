d5d7e2f094bc48f4f1e5123233952285991676c7
Ezequias Ferreira <ezequiasbarros1997@gmail.com>
2022-04-04 21:10:01 -0300

    Agregação das entidades

--- modulo-4-mongodb/homework1/home1.js
+++ modulo-4-mongodb/homework1/home1.js
@@ -1,66 +1,88 @@
 //Criação da collection users 
 //Uso da função db.users.insertMany()
+
 db.users.insertMany([
-	{
-		"username" : "flaviosobrinho",
-		"password" : "$2a$10$wex6tSCNcfd2m.LGkhnTIunv7eVk7JSrjBmM\/o9rFec.QfZ\/6Lltm",
-		"email" : "flavio@gmail.com",
-		"isactive" : true,
-	},
-	{
-		"username" : "ezeq21",
-		"password" : "$2a$10$TZ3RaSkBh3u5LTomUF7sM.zJcUcu4QAKhj5dF1UmZ5Bnr35GjJtZ.",
-		"email" : "ezequias@gmail.com",
-			"isactive" : true,
-	},
-	{
-		"username" : "carlosalm",
-		"password" : "$2a$10$.P7v9IHm61IX8Fut2K9\/RuTX3Pi6fN4uZso0Shl2tNjulb7Em7gz.",
-		"email" : "teste@gmail.com",
-		"isactive" : false,
-	},
-	{
-		"username" : "maicon23",
-		"password" : "$2a$10$Sji0vFz3Q6ouzudYBFqRc.1jSHJdmRSogkCLL1lV5i.N9bFyzjaF2",
-		"email" : "string@gmail.com",
-		"isactive" : true,
-	}
-])
-
-//inserção da collection de receitas
-db.recipes.insertMany([
-	{
-		"recipename" : "Pão com ovo",
-		"image" : "",
-		"preparerecipe" : "Lorem Ipsum is simply dummy text of the printing",
-		"preparetime" : 12,
-		"price" : 10.43,
-		"calories" : 3241.00,
-		"userid" : ObjectId('624b5056598180f29b29f1db'),
-		"ingredients":["pão", "ovo"]
-	},
-	{
-		"recipename" : "Camarão com leite",
-		"image" : "",
-		"preparerecipe" : "Lorem Ipsum is simply dummy text of the printing",
-		"preparetime" : 15,
-		"price" : 32.43,
-		"calories" : 321.00,
-		"userid" : ObjectId('624b5056598180f29b29f1db'),
-		"ingredients":["camarão", "leite de côco"]
-	},
-	{
-		"recipename" : "Ovo com queijo",
-		"image" : "",
-		"preparerecipe" : "Lorem Ipsum is simply dummy text of the printing",
-		"preparetime" : 32,
-		"price" : 12.45,
-		"calories" : 213.00,
-		"userid" : ObjectId('624b5056598180f29b29f1de'),
-		"ingredients":["ovo", "queijo"]
-	}
-])
-
+{
+	"username" : "flaviosobrinho",
+	"password" : "$2a$10$wex6tSCNcfd2m.LGkhnTIunv7eVk7JSrjBmM\/o9rFec.QfZ\/6Lltm",
+	"email" : "flavio@gmail.com",
+	"isactive" : true,
+	"role": "ROLE_STANDARD",
+	"recipesCreated":[]
+}
+,
+{
+	"username" : "ezeq_ferreira",
+	"password" : "$2a$10$wex6tSCNcfd2m.LGkhnTIunv7eVk7JSrjBmM\/o9rFec.QfZ\/6Lltm",
+	"email" : "ezeq@gmail.com",
+	"isactive" : false,
+	"role": "ROLE_PREMIUM",
+	"recipesCreated":
+	[
+		{			
+			"recipename":"Camarão no alho e óleo",
+			"image":"",
+			"preparerecipe":"Lorem Ipsum is simply dummy text of the printing",
+			"preparetime": 32,
+			"price": 15.67,
+			"calories": 1256,
+			"ingredients":["camarão", "alho", "óleo"],
+			"classification":
+			[
+				{
+					"userid": "",
+					"stars": 5,
+					"coment": "Lorem Ipsum is simply dummy text of the printing"
+				}
+			]
+		}
+	]
+}
+,
+{
+	"username" : "maic321",
+	"password" : "$2a$10$wex6tSCNcfd2m.LGkhnTIunv7eVk7JSrjBmM\/o9rFec.QfZ\/6Lltm",
+	"email" : "maicon@gmail.com",
+	"isactive" : true,
+	"role": "ROLE_PREMIUM",
+	"recipesCreated":
+	[
+		{			
+			"recipename":"Bolinho de chuva",
+			"image":"",
+			"preparerecipe":"Lorem Ipsum is simply dummy text of the printing",
+			"preparetime": 43,
+			"price": 32.32,
+			"calories": 1234,
+			"ingredients":["ovo", "leite", "manteiga", "açucar"],
+			"classification":
+			[
+				{
+					"userid": "",
+					"stars": 3.7,
+					"coment": "Lorem Ipsum is simply dummy text of the printing"
+				}
+			]
+		},
+		{			
+			"recipename":"Acarajé",
+			"image":"",
+			"preparerecipe":"Lorem Ipsum is simply dummy text of the printing",
+			"preparetime": 132,
+			"price": 24.56,
+			"calories": 2500,
+			"ingredients":["ovo", "camarão", "óleo de dênde", "feijão"],
+			"classification":
+			[
+				{
+					"userid": "",
+					"stars": 4.6,
+					"coment": "Lorem Ipsum is simply dummy text of the printing"
+				}
+			]
+		}
+	]
+}])
 
 //Encontrar todos
 db.users.find().pretty()
@@ -68,7 +90,7 @@ db.users.find().pretty()
 //Encontrar apenas o especificado
 db.users.find(
 { 
-  "username" : "maicon23"
+  "username" : "maic321"
 }
 ).pretty()
 
@@ -76,15 +98,15 @@ db.users.find(
 
 db.users.find({
     $or : [
-        {"username" : "maicon23"},
+        {"username" : "maic231"},
         {"isactive": true}    
     ]
 }).pretty()
 
 // usando o IN
 
-db.recipes.find({
-    "ingredients" : {
+db.users.find({
+    "recipesCreated.ingredients" : {
         $in : ["ovo", "camarão"]
         }
 })
